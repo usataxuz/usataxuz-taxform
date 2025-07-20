bf0569360796c3f30d771ba19ccd3d94df793040
Abdurashid Turgunov <dscabdurashid@Mac.lan>
2025-07-20 00:28:24 -0400

    App.tsx fayli yangilandi

--- App.tsx
+++ App.tsx
@@ -1,29 +1,20 @@
-import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from './auth';
-import TaxForm from './TaxForm';
-import Login from './Login';
-import Logout from './Logout';
+{\rtf1\ansi\ansicpg1251\cocoartf2822
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
 
-function App() {
-  const [user, loading] = useAuthState(auth);
-
-  if (loading) return <p>Yuklanmoqda...</p>;
-
-  return (
-    <div className="min-h-screen bg-gray-50 p-4">
-      {user ? (
-        <>
-          <div className="text-right mb-4">
-            <Logout />
-          </div>
-          <TaxForm />
-        </>
-      ) : (
-        <Login />
-      )}
-    </div>
-  );
+\f0\fs24 \cf0 import React from "react";\
+import TaxForm from "./TaxForm";\
+\
+function App() \{\
+  return (\
+    <div className="min-h-screen bg-gray-100 py-10 px-4">\
+      <TaxForm />\
+    </div>\
+  );\
+\}\
+\
+export default App;\
 }
-
-export default App;
