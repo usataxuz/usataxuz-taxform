7a23572198de4b91f3ffe41849dc6ddda36490f6
Abdurashid Turgunov <dscabdurashid@Mac.lan>
2025-07-20 00:45:51 -0400

    main.tsx fayli src papkaga ko‘chirildi

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+{\rtf1\ansi\ansicpg1251\cocoartf2822
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+\f0\fs24 \cf0 // src/main.tsx\
+import React from 'react'\
+import ReactDOM from 'react-dom/client'\
+import App from './App'\
+import './index.css'\
+\
+ReactDOM.createRoot(document.getElementById('root')!).render(\
+  <React.StrictMode>\
+    <App />\
+  </React.StrictMode>\
+)\
+}
