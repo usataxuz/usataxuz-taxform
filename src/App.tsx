1437f91edb17e78047ca577db1bcf70ca458e2b9
Abdurashid Turgunov <dscabdurashid@Mac.lan>
2025-07-20 01:42:02 -0400

    fix: update Auth import path

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import React from "react";
-import TaxForm from "./src/TaxForm";
-// yoki
-import TaxForm from "./src/components/TaxForm"; // agar `TaxForm.tsx` `components` papkada bo‘lsa
-// yoki
-import TaxForm from "./TaxForm"; // faqat **agar** `App.tsx` bilan bir papkada bo‘lsa
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "./auth";
-import Login from "./Login";
-import Logout from "./Logout";
+// src/App.tsx
+import Auth from './auth';
 
 function App() {
-  const [user] = useAuthState(auth);
   return (
-    <div className="p-4">
-      {user ? (
-        <>
-          <Logout />
-          <TaxForm />
-        </>
-      ) : (
-        <Login />
-      )}
+    <div>
+      <Auth />
     </div>
   );
 }
